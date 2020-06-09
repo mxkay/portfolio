79bac1e0c0254ddbf403a19ae8c5667e7aaaba01
mxkay <kayalyxvail@gmail.com>
2020-06-09 15:30:10 -0400

    imported project data and built project cards

---
+++ script.js
@@ -0,0 +1,51 @@
+const sheetURL = 'https://docs.google.com/spreadsheets/d/12WudbKp3rSdJJpJx7ANPEcC8J7unsYWwWaHBpXjtfPo/edit?usp=sharing';
+
+const sheetAsJSON = 'https://spreadsheets.google.com/feeds/list/12WudbKp3rSdJJpJx7ANPEcC8J7unsYWwWaHBpXjtfPo/od6/public/values?alt=json'
+
+// fetch project data
+$.ajax({
+    url: sheetAsJSON,
+})
+// then create an array containing each project's title, description, image, and url
+.then( data => {
+    // console.log('data', data)
+    const projects = data.feed.entry.map( project => {
+        return {
+            title: project.gsx$title.$t,
+            description: project.gsx$description.$t,
+            image: project.gsx$image.$t,
+            url: project.gsx$url.$t
+        }
+    })
+
+    // initialize array for project card JQuery elements
+    $projects = [];
+
+    // build a JQuery element for each project
+    projects.forEach( (project, index) => {
+        $projects.push($(`
+            <div class="card" id="project${index}">
+                <div class="image">
+                    <h2 class="title">${project.title}</h2>
+                    <div class="description">${project.description}</div>
+                </div>
+            </div>
+            `)
+            // add background image
+            .css('background-image', `url(${project.image})`));
+        $('#portfolio > .wrapper').append($projects[index]);
+        // $(`#project${index} > .image`).css('background', `url(${project.url}`);
+    })
+
+    console.log($projects);
+})
+.catch( err => console.log('error loading ajax'));
+
+/* template for project card
+    <div class="card" id="project1">
+        <div class="image">
+            <h2 class="title">Title</h2>
+            <div class="description">Lorem ipsum dolor sit amet consectetur adipisicing elit. Eum vero neque voluptatem, repellat labore odio error ut minima!</div>
+        </div>
+    </div>
+*/
