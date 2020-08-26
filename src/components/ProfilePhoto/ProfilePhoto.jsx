bc068669c72e76b06dc6381a24cfb8516ae9f4aa
mxkay <kayalyxvail@gmail.com>
2020-08-26 03:45:55 -0400

    Built ProfilePhoto

---
+++ src/components/ProfilePhoto/ProfilePhoto.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./ProfilePhoto.css";
+
+const ProfilePhoto = () =>
+    <div className="profile-photo-wrapper">
+        <div className="profile-photo-frame">
+            <div className="profile-photo"/>
+        </div>
+    </div>
+
+export default ProfilePhoto;
