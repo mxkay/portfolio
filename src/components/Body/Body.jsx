35b16df340a2ceab12e4ab286c1b0153368263cd
mxkay <kayalyxvail@gmail.com>
2020-08-26 09:41:37 -0400

    Added custom scrollbar to Body

--- src/components/Body/Body.jsx
+++ src/components/Body/Body.jsx
@@ -1,9 +1,14 @@
 import React from "react";
+import { Scrollbars } from "react-custom-scrollbars";
 import "./Body.css";
 
 const Body = props => 
     <div className="body">
-        {props.children}
+        {props.nav ? props.nav : <></>}
+        <Scrollbars>
+            {props.children}
+        </Scrollbars>
+
     </div>
 
 export default Body;
