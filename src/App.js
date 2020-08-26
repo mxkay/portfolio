258da49fab6bef470cc8934f8b79a63f57fd7060
mxkay <kayalyxvail@gmail.com>
2020-08-26 02:27:58 -0400

    Added Background to App

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import './App.css';
 
+import Background from "./components/Background/Background"
+
 function App() {
   return (
     <div className="App">
+      <Background>
+        <p>Test</p>
+      </Background>
     </div>
   );
 }
