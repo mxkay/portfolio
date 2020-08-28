a80de2b783b4f06ae0489c504d8de099f6db2a96
mxkay <kayalyxvail@gmail.com>
2020-08-28 15:35:51 -0400

    Simplified ProfilePhoto styling

--- src/components/ProfilePhoto/ProfilePhoto.jsx
+++ src/components/ProfilePhoto/ProfilePhoto.jsx
@@ -1,11 +1,7 @@
 import React from "react";
 import "./ProfilePhoto.css";
 
-const ProfilePhoto = () =>
-    <div className="profile-photo-wrapper">
-        <div className="profile-photo-frame">
-            <div className="profile-photo"/>
-        </div>
-    </div>
+const ProfilePhoto = ({ src }) =>
+    <img className="profile-photo" src={src}/>
 
 export default ProfilePhoto;
