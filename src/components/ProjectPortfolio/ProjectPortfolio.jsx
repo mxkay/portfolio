75e5c5e9a8b636bfbcbd5a23f52af691a1b28439
mxkay <kayalyxvail@gmail.com>
2020-08-28 15:37:47 -0400

    Improved fetch URL

--- src/components/ProjectPortfolio/ProjectPortfolio.jsx
+++ src/components/ProjectPortfolio/ProjectPortfolio.jsx
@@ -9,14 +9,14 @@ const ProjectPortfolio = () => {
 
     // ON MOUNT
     useEffect( () => {
-        // const sheetURL = 'https://docs.google.com/spreadsheets/d/12WudbKp3rSdJJpJx7ANPEcC8J7unsYWwWaHBpXjtfPo/edit?usp=sharing';
-        const sheetAsJSON = 'https://spreadsheets.google.com/feeds/list/12WudbKp3rSdJJpJx7ANPEcC8J7unsYWwWaHBpXjtfPo/od6/public/values?alt=json'
+        // const sheetURL = 'https://docs.google.com/spreadsheets/d/12WudbKp3rSdJJpJx7ANPEcC8J7unsYWwWaHBpXjtfPo/edit#gid=677653555';
+        const sheetAsJSON = 'https://spreadsheets.google.com/feeds/list/12WudbKp3rSdJJpJx7ANPEcC8J7unsYWwWaHBpXjtfPo/1/public/full?alt=json'
 
         // FETCH project data
         axios(sheetAsJSON)
         // STORE project data to state
         .then( response => {
-            // console.log("response.data", response.data);
+            // console.log("response", response.data.feed.entry);
             updateProjects( response.data.feed.entry.map( project => {
                 return {
                     title: project.gsx$title.$t,
