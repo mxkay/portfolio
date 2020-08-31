094d17c9fb4aeb6de6f52520e0abbb2d241c85fe
mxkay <kayalyxvail@gmail.com>
2020-08-31 00:08:50 -0400

    Implemented ProfilePhoto in Profile

--- src/components/Profile/Profile.presenter.jsx
+++ src/components/Profile/Profile.presenter.jsx
@@ -1,12 +1,12 @@
 import React from "react";
+import ProfilePhoto from "../ProfilePhoto/ProfilePhoto";
+
 import "./Profile.css";
 
 const ProfilePresenter = ({ name, pronouns, title, photo }) =>
     <div className="profile-wrapper">
         <div className="profile">
-            <div className="profile-photo-wrapper">
-                <img className="profile-photo" src={photo} />
-            </div>
+            <ProfilePhoto src={photo} />
             <div className="profile-name">{name}</div>
             <div className="profile-pronouns">{pronouns}</div>
             <div className="profile-title">{title}</div>
