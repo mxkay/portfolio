78b32e817131865bc5b7e48e7aa3c29f7e01fd14
mxkay <kayalyxvail@gmail.com>
2020-09-02 18:08:12 -0400

    Cnaged naming convention to .container for smart components

---
+++ src/components/Profile/Profile.container.jsx
@@ -0,0 +1,39 @@
+import React, { useState, useEffect } from "react";
+import axios from "axios";
+import Profile from "./Profile";
+
+const ProfileContainer = () => {
+
+    const [profileData, updateProfileData] = useState({
+        name: "",
+        pronouns: "",
+        title: "",
+        photo: "",
+    });
+
+    // ON MOUNT
+    useEffect( () => {
+        // const sheetURL = 'https://docs.google.com/spreadsheets/d/12WudbKp3rSdJJpJx7ANPEcC8J7unsYWwWaHBpXjtfPo/edit#gid=677653555';
+        const sheetAsJSON = 'https://spreadsheets.google.com/feeds/list/12WudbKp3rSdJJpJx7ANPEcC8J7unsYWwWaHBpXjtfPo/2/public/full?alt=json'
+
+        // FETCH project data
+        axios(sheetAsJSON)
+        // STORE project data to state
+        .then( response => {
+            // console.log("response.data", response.data);
+            updateProfileData( 
+                {
+                    name: response.data.feed.entry[0].gsx$name.$t,
+                    pronouns: response.data.feed.entry[0].gsx$pronouns.$t,
+                    title: response.data.feed.entry[0].gsx$title.$t,
+                    photo: response.data.feed.entry[0].gsx$photo.$t,
+                }
+            );
+        })
+        .catch( err => console.log('error loading ajax'));
+    },[]);
+
+    return <Profile {...profileData} />;
+}
+
+export { ProfileContainer };
--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,39 +1,18 @@
-import React, { useState, useEffect } from "react";
-import axios from "axios";
-import ProfilePresenter from "./Profile.presenter";
+import React from "react";
+import ProfilePhoto from "../ProfilePhoto/ProfilePhoto";
+import { LinkListContainer as LinkList } from "../LinkList/LinkList.container";
 
-const Profile = () => {
+import "./Profile.css";
 
-    const [profileData, updateProfileData] = useState({
-        name: "",
-        pronouns: "",
-        title: "",
-        photo: "",
-    });
-
-    // ON MOUNT
-    useEffect( () => {
-        // const sheetURL = 'https://docs.google.com/spreadsheets/d/12WudbKp3rSdJJpJx7ANPEcC8J7unsYWwWaHBpXjtfPo/edit#gid=677653555';
-        const sheetAsJSON = 'https://spreadsheets.google.com/feeds/list/12WudbKp3rSdJJpJx7ANPEcC8J7unsYWwWaHBpXjtfPo/2/public/full?alt=json'
-
-        // FETCH project data
-        axios(sheetAsJSON)
-        // STORE project data to state
-        .then( response => {
-            // console.log("response.data", response.data);
-            updateProfileData( 
-                {
-                    name: response.data.feed.entry[0].gsx$name.$t,
-                    pronouns: response.data.feed.entry[0].gsx$pronouns.$t,
-                    title: response.data.feed.entry[0].gsx$title.$t,
-                    photo: response.data.feed.entry[0].gsx$photo.$t,
-                }
-            );
-        })
-        .catch( err => console.log('error loading ajax'));
-    },[]);
-
-    return <ProfilePresenter {...profileData} />;
-}
+const Profile = ({ name, pronouns, title, photo }) =>
+    <div className="profile-wrapper">
+        <div className="profile">
+            <ProfilePhoto src={photo} />
+            <div className="profile-name">{name}</div>
+            <div className="profile-pronouns">{pronouns}</div>
+            <div className="profile-title">{title}</div>
+            <LinkList />
+        </div>
+    </div>
 
 export default Profile;
