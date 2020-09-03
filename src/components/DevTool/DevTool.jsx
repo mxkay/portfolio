60671380bc8e603aeae280bdb9b2f4f3f895953f
mxkay <kayalyxvail@gmail.com>
2020-09-02 23:30:40 -0400

    Added aliases and some new icons

--- src/components/DevTool/DevTool.jsx
+++ src/components/DevTool/DevTool.jsx
@@ -1,39 +1,47 @@
 import React from "react";
-
-// get icons from https://react-icons.github.io/icons?name=di
 import DevToolIcon from "./DevToolIcon";
-import { DiJsBadge } from "react-icons/di";
-import { DiHtml5 } from "react-icons/di";
-import { DiCss3 } from "react-icons/di";
-import { DiRuby } from "react-icons/di";
-import { DiJqueryLogo } from "react-icons/di";
-import { BiTransferAlt } from "react-icons/bi";
-import { DiReact } from "react-icons/di";
-import { DiBootstrap } from "react-icons/di";
-import { FaCircle } from "react-icons/fa";
+
+// get icons from https://react-icons.github.io/react-icons/
+import { DiJsBadge as JavaScriptIcon } from "react-icons/di";
+import { DiHtml5 as HTML5Icon } from "react-icons/di";
+import { DiCss3 as CSS3Icon } from "react-icons/di";
+import { DiRuby as RubyIcon } from "react-icons/di";
+import { SiRails as RailsIcon } from "react-icons/si";
+import { DiJqueryLogo as JQueryIcon } from "react-icons/di";
+import { BiTransferAlt as AxiosIcon } from "react-icons/bi";
+import { DiReact as ReactIcon } from "react-icons/di";
+import { SiExpo as ExpoIcon } from "react-icons/si";
+import { DiBootstrap as BootstrapIcon } from "react-icons/di";
+import { FaCircle as CircleIcon } from "react-icons/fa";
 
 import "./DevTool.css";
 
 const DevTool = ({ name }) =>
     <div className="dev-tool-wrapper">
         { name === "JavaScript" ?
-            <DevToolIcon icon={<DiJsBadge />} color="#F1E05A"/>
+            <DevToolIcon icon={<JavaScriptIcon />} color="#F1E05A"/>
         : name === "HTML5" ?
-            <DevToolIcon icon={<DiHtml5 />} color="#E44D26"/>
+            <DevToolIcon icon={<HTML5Icon />} color="#E44D26"/>
         : name === "CSS3" ?
-            <DevToolIcon icon={<DiCss3 />} color="#384DE4"/>
+            <DevToolIcon icon={<CSS3Icon />} color="#384DE4"/>
         : name === "Ruby" ?
-            <DevToolIcon icon={<DiRuby />} color="#701516"/>
+            <DevToolIcon icon={<RubyIcon />} color="#701516"/>
+        : name === "Rails" ?
+            <DevToolIcon icon={<RailsIcon />} color="#CC2100"/>
         : name === "jQuery" ?
-            <DevToolIcon icon={<DiJqueryLogo />} color="#3372A2"/>
+            <DevToolIcon icon={<JQueryIcon />} color="#3372A2"/>
         : name === "axios" ?
-            <DevToolIcon icon={<BiTransferAlt />} color="#854195"/>
+            <DevToolIcon icon={<AxiosIcon />} color="#854195"/>
         : name === "React" ?
-            <DevToolIcon icon={<DiReact />} color="#60D8FF"/>
+            <DevToolIcon icon={<ReactIcon />} color="#60D8FF"/>
+        : name === "React Native" ?
+            <DevToolIcon icon={<ReactIcon />} color="#60D8FF"/>
         : name === "Bootstrap" ?
-            <DevToolIcon icon={<DiBootstrap />} color="#563D7C"/>
+            <DevToolIcon icon={<BootstrapIcon />} color="#563D7C"/>
+        : name === "Expo" ?
+            <DevToolIcon icon={<ExpoIcon />} color="#020020"/>
         : // default
-            <DevToolIcon icon={<FaCircle />} color="#000"/>
+            <DevToolIcon icon={<CircleIcon />} color="#000"/>
         }
         <div className="dev-tool-name">
             {name}
