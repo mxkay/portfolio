2198cbb3de6ca4104889bff230bebf3c201507a5
mxkay <kayalyxvail@gmail.com>
2020-09-05 23:22:45 -0400

    Added styling to About bio

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,9 +1,20 @@
 import React from 'react'
+import { Figure } from "react-bootstrap";
+import DevToolList from "../DevToolList/DevToolList";
+
 import "./About.css";
 
 const About = ({ bio }) => 
     <div className="about-bio">
-        {bio}
+        {bio && bio[0] ? bio.map( (textBlock, index) =>
+            <Figure className={`about-bio-text-block ${textBlock.variant}`} key={index}>
+                    <>{textBlock.variant === "devToolList" ?
+                        <DevToolList toolNames={textBlock.content.split(',')} />
+                    : 
+                        textBlock.content
+                    }</>
+            </Figure>
+        ) : <></>}
     </div>
 
 export default About;
