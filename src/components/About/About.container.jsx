3603934c6e4c4945a3bf06cc60ce400c265f855a
mxkay <kayalyxvail@gmail.com>
2020-09-05 23:01:08 -0400

    Modified fetch to store variant (used for styling) and content as bio state

--- src/components/About/About.container.jsx
+++ src/components/About/About.container.jsx
@@ -3,19 +3,24 @@ import axios from "axios";
 import About from "./About";
 
 const AboutContainer = () => {
-    const [ bio, setBio ] = useState();
+    const [ bio, setBio ] = useState({});
 
     // ON MOUNT
     useEffect( () => {
         // const sheetURL = 'https://docs.google.com/spreadsheets/d/12WudbKp3rSdJJpJx7ANPEcC8J7unsYWwWaHBpXjtfPo/edit#gid=677653555';
-        const sheetAsJSON = 'https://spreadsheets.google.com/feeds/list/12WudbKp3rSdJJpJx7ANPEcC8J7unsYWwWaHBpXjtfPo/2/public/full?alt=json'
+        const sheetAsJSON = 'https://spreadsheets.google.com/feeds/list/12WudbKp3rSdJJpJx7ANPEcC8J7unsYWwWaHBpXjtfPo/3/public/full?alt=json'
 
         // FETCH bio data
         axios(sheetAsJSON)
         // STORE bio to state
         .then( response => {
-            // console.log("response.data", response.data);
-            setBio( response.data.feed.entry[0].gsx$bio.$t );
+            // console.log("response", response.data.feed.entry);
+            setBio( response.data.feed.entry.map( link => {
+                return {
+                    variant: link.gsx$variant.$t,
+                    content: link.gsx$content.$t
+                }
+            }));
         })
         .catch( err => console.log('error loading ajax'));
     },[]);
