baa1b2bd4c960d2a623a6a8b1ef04e50e703be6a
mxkay <kayalyxvail@gmail.com>
2020-09-07 22:39:52 -0400

    Added contact data to Contact presenter

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,8 +1,10 @@
 import React from "react";
 
-const Contact = () => 
-    <div>
-        contact
+const Contact = ({ name, pronouns, email }) => 
+    <div className="contact">
+        <div className="contact-name">{name}</div>
+        <div className="contact-pronouns">{pronouns}</div>
+        <div className="contact-email">{email}</div>
     </div>
 
 export default Contact;
