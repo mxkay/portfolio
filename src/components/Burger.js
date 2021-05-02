fc1d861011345d66ddf6b9c472a917cd900634ad
Mathilda <me@mathilda.dev>
2021-05-01 23:03:13 -0400

    Build burger

---
+++ src/components/Burger.js
@@ -0,0 +1,64 @@
+import styled from 'styled-components';
+
+const StyledBurger = styled.button`
+  font-size: 2rem;
+  position: absolute;
+  top: 0;
+  left: 0;
+  padding: 0.4em;
+  border: 0.2em solid rgba(0,0,0,0);
+  background: transparent;
+  z-index: 10;
+  cursor: pointer;
+  outline: none;
+  
+  :focus {
+    border-radius: 0.2em;
+    border-color: #DEF #456 #456 #DEF;
+  }
+
+  &>div {
+    width: 1.8em;
+    height: 1.8em;
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+    
+    &>div {
+      position: relative;
+      width: 2em;
+      height: 0.2em;
+      border-radius: 0.1em;
+      transition: all 0.3s linear;
+      background: #FFF;
+
+      &:first-child {
+        transform: ${({ open }) => open ? 'translateY(0.8em) rotate(45deg)' : 'rotate(0)'};
+      }
+      
+      &:nth-child(2) {
+        opacity: ${({ open }) => open ? '0' : '1'};
+        transform: ${({ open }) => open ? 'translateX(-2em)' : 'translateX(0)'};
+      }
+      
+      &:nth-child(3) {
+        transform: ${({ open }) => open ? 'translateY(-0.8em) rotate(-45deg)' : 'rotate(0)'};
+      }
+    }
+  }
+`;
+
+const Burger = ({ open, setOpen }) => {
+  return (
+    <StyledBurger open={open} onClick={() => setOpen(!open)}>
+      <div>
+        <div />
+        <div />
+        <div />
+      </div>
+    </StyledBurger>
+  );
+};
+
+export default Burger;
