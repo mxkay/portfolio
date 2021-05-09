c46b5f37ba56d51bb56087af057bc15af27259fa
Mathilda <me@mathilda.dev>
2021-05-09 17:22:45 -0400

    Add content to about page

--- src/pages/about.js
+++ src/pages/about.js
@@ -11,18 +11,35 @@ const About = () => (
     <Menu />
     <Gate>
       <Container scroll cover background='linear-gradient(90deg, transparent 0%, rgba(0,0,0,0.7) 10%, rgba(0,0,0,0.7) 90%, transparent 100%);' >
-        <h1>About me:</h1>
+        <h1>About me</h1>
         <h2>Fullstack software engineer</h2>
-        <p>With experience across in a wide variety of languages and frameworks.</p>
-        <p>Enjoys building applications that are accessibly, robust, operable, and performant.</p>
-        <h2>Avid learner & passionate educator</h2>
-        <p>Always looking to improve my skills and tap into the most modern technologies.</p>
-        <p>Teaching what I know to the next generation of software engineers.</p>
+        <p>Enjoys building applications that are accessible, robust, and performant.</p>
+        
+        <div style={{ padding: '1em 1em 0 2em' }}>
+          <h3>Web and mobile applications</h3>
+          <p>Makes your idea come to life, using modern frameworks like React or React Native.</p>
+          <h3>Backend solutions</h3>
+          <p>Builds performant APIs that make sense and get the job done.</p>
+          <p>Skilled with both SQL and no-SQL databases.</p>
+        </div>
+        <h2>Educator</h2>
+        <p>Enjoys teaching others how to write code, solve algorithms, and build applications.</p>
+        <p>
+          Delivering
+          {' '}
+          <a
+            href='https://generalassemb.ly/education/software-engineering-immersive-remote'
+            target='_blank'
+            rel="noreferrer"
+          >
+            General Assembly's Software Engineering Immersive Remote course
+          </a>.
+        </p>
         <h2>Lover of math, maker of art</h2>
         <p>Visual artist specializing in the application of mathematics to create stunning works.</p>
         <p>Carrying my passion for math and art into everything I do to find elegant solutions to complex problems.</p>
         <h2>Community oriented</h2>
-        <p>Passionate about volunteering, community organizing, and human rights advocacy.</p>
+        <p>Passionate about non-profit work, community organizing, and human rights advocacy.</p>
         <p>Ask me about my side projects.</p>
       </Container>
     </Gate>
